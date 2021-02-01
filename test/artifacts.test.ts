63fbcabaee08cedd1050887f1065bddbc468c983
dvaJi <francisco_p@outlook.com>
2021-02-01 01:10:00 -0300

    chore: add unit tests for japanese artifacts

--- test/artifacts.test.ts
+++ test/artifacts.test.ts
@@ -1,10 +1,13 @@
 import GenshinData from '../src';
 
+let currentArtifacts = 0;
+
 describe('Artifacts English', () => {
   it('should return all artifacts', async () => {
     const genshinData = new GenshinData({ language: 'english' });
     const artifacts = await genshinData.artifacts();
-    expect(artifacts.length).toEqual(31);
+    expect(artifacts.length).toBeGreaterThan(0);
+    currentArtifacts = artifacts.length;
   });
 
   it('should contains all mandatory fields', async () => {
@@ -22,11 +25,29 @@ describe('Artifacts Spanish', () => {
   it('should return all artifacts', async () => {
     const genshinData = new GenshinData({ language: 'spanish' });
     const artifacts = await genshinData.artifacts();
-    expect(artifacts.length).toEqual(31);
+    expect(artifacts.length).toEqual(currentArtifacts);
   });
 
   it('should contains all mandatory fields', async () => {
     const genshinData = new GenshinData({ language: 'spanish' });
+    const artifacts = await genshinData.artifacts();
+
+    for (const artifact of artifacts) {
+      expect(artifact.id).toBeDefined();
+      expect(artifact.name).toBeDefined();
+    }
+  });
+});
+
+describe('Artifacts Japanese', () => {
+  it('should return all artifacts', async () => {
+    const genshinData = new GenshinData({ language: 'japanese' });
+    const artifacts = await genshinData.artifacts();
+    expect(artifacts.length).toEqual(currentArtifacts);
+  });
+
+  it('should contains all mandatory fields', async () => {
+    const genshinData = new GenshinData({ language: 'japanese' });
     const artifacts = await genshinData.artifacts();
 
     for (const artifact of artifacts) {
