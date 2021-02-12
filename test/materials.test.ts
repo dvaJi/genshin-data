4f4cf2207ad1aedef209a6bb8512ea4086fd2329
dvaJi <francisco_p@outlook.com>
2021-02-12 15:53:03 -0300

    japanese(materials): add all current materials in japanese

--- test/materials.test.ts
+++ test/materials.test.ts
@@ -1,10 +1,13 @@
 import GenshinData from '../src';
 
+let currentMaterials = 0;
+
 describe('Materials English', () => {
   it('should return all materials', async () => {
     const genshinData = new GenshinData({ language: 'english' });
     const materials = await genshinData.materials();
-    expect(materials.length).toEqual(118);
+    expect(materials.length).toBeGreaterThan(0);
+    currentMaterials = materials.length;
   });
 
   it('should contains all mandatory fields', async () => {
@@ -22,11 +25,29 @@ describe('Materials Spanish', () => {
   it('should return all materials', async () => {
     const genshinData = new GenshinData({ language: 'spanish' });
     const materials = await genshinData.materials();
-    expect(materials.length).toEqual(118);
+    expect(materials.length).toEqual(currentMaterials);
   });
 
   it('should contains all mandatory fields', async () => {
     const genshinData = new GenshinData({ language: 'spanish' });
+    const materials = await genshinData.materials();
+
+    for (const material of materials) {
+      expect(material.id).toBeDefined();
+      expect(material.name).toBeDefined();
+    }
+  });
+});
+
+describe('Materials Japanese', () => {
+  it('should return all materials', async () => {
+    const genshinData = new GenshinData({ language: 'japanese' });
+    const materials = await genshinData.materials();
+    expect(materials.length).toEqual(currentMaterials);
+  });
+
+  it('should contains all mandatory fields', async () => {
+    const genshinData = new GenshinData({ language: 'japanese' });
     const materials = await genshinData.materials();
 
     for (const material of materials) {
