36b5a3e460e4b8367f87e2442ea8c7c6243f05b9
dvaJi <francisco_p@outlook.com>
2021-04-15 17:28:41 -0400

    fix(characters): format and organize attribute skills and update unit test

--- test/characters.test.ts
+++ test/characters.test.ts
@@ -36,6 +36,27 @@ describe('Characters English', () => {
       expect(character.passives.length).toBeGreaterThan(0);
     }
   });
+
+  it('all skills must contains 15 attribute values', async () => {
+    const characters = await genshinData.characters();
+
+    for (const character of characters) {
+      for (const skill of character.skills) {
+        // Mona SP skill must be skipped
+        if (skill.id === 'illusory_torrent') {
+          continue;
+        }
+
+        for (const attribute of skill.attributes) {
+          if (attribute[1].length < 15) {
+            fail(
+              `Skill [${skill.id}] attribute [${attribute[0]}] of ${character.id} must have 15 attributes`
+            );
+          }
+        }
+      }
+    }
+  });
 });
 
 describe('Characters Spanish', () => {
@@ -57,6 +78,27 @@ describe('Characters Spanish', () => {
       expect(character.passives.length).toBeGreaterThan(0);
     }
   });
+
+  it('all skills must contains 15 attribute values', async () => {
+    const characters = await genshinData.characters();
+
+    for (const character of characters) {
+      for (const skill of character.skills) {
+        // Mona SP skill must be skipped
+        if (skill.id === 'illusory_torrent') {
+          continue;
+        }
+
+        for (const attribute of skill.attributes) {
+          if (attribute[1].length < 15) {
+            fail(
+              `Skill [${skill.id}] attribute [${attribute[0]}] of ${character.id} must have 15 attributes`
+            );
+          }
+        }
+      }
+    }
+  });
 });
 
 describe('Characters Japanese', () => {
@@ -76,6 +118,27 @@ describe('Characters Japanese', () => {
       expect(character.description).toBeDefined();
       expect(character.skills.length).toBeGreaterThan(0);
       expect(character.passives.length).toBeGreaterThan(0);
+    }
+  });
+
+  it('all skills must contains 15 attribute values', async () => {
+    const characters = await genshinData.characters();
+
+    for (const character of characters) {
+      for (const skill of character.skills) {
+        // Mona SP skill must be skipped
+        if (skill.id === 'illusory_torrent') {
+          continue;
+        }
+
+        for (const attribute of skill.attributes) {
+          if (attribute[1].length < 15) {
+            fail(
+              `Skill [${skill.id}] attribute [${attribute[0]}] of ${character.id} must have 15 attributes`
+            );
+          }
+        }
+      }
     }
   });
 });
